4bb791733c2091346ff8bf61bfc3e0719c44f1ed
bertybuttface <110790513+bertybuttface@users.noreply.github.com>
2023-04-15 10:16:22 +0100

    Add checks for missing identity

--- zititunneller/rootfs/etc/cont-init.d/ziti-edge-tunnel.sh
+++ zititunneller/rootfs/etc/cont-init.d/ziti-edge-tunnel.sh
@@ -29,6 +29,19 @@ if [ ! -x /opt/ziti-edge-tunnel ]; then
   exit 1
 fi
 
-# Enroll identity
-echo "${JWT}" > /identities/default.jwt
-/opt/ziti-edge-tunnel enroll --jwt /identities/default.jwt --identity /identities/default.json
+# Check if JWT env is set and not empty
+if [ -n "${JWT}" ] && [ "${JWT}" != "null" ]; then
+  echo "JWT is set, enrolling new identity"
+  # Enroll identity
+  echo "${JWT}" > /identities/default.jwt
+  /opt/ziti-edge-tunnel enroll --jwt /identities/default.jwt --identity /identities/default.json
+else
+  echo "JWT not set, using stored identity if there is one"
+  # Check if /identities/default.json exists, if it doesn't error out
+  if [ -f "/identities/default.json" ]; then
+    echo "Stored identity found at /identities/default.json"
+  else
+    echo "Error: No stored identity found at /identities/default.json" >&2
+    exit 1
+  fi
+fi
